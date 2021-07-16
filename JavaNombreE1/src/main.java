62b5117bc45edade92418dc50b59d622345d0250
Tamara Morcans <tamaramorcans@comunidad.unam.mx>
2021-07-15 23:31:25 -0500

    EjercicioNombre-Java

---
+++ JavaNombreE1/src/main.java
@@ -0,0 +1,24 @@
+
+import java.util.*;
+ 
+    public class main {
+
+            public static void main (String [ ] args){
+
+            Scanner objeto= new Scanner (System.in);
+ 
+ String nombre;
+ int edad;
+
+System.out.println("¡Hola! ¿Cómo te llamas?");
+nombre=objeto.nextLine( );
+
+
+
+
+System.out.println ("Bienvenido " +nombre+ "") ;
+
+}
+}
+
+//Listo
